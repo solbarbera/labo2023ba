3d28f76622ff062f38ba2fbec43817cb23abbc9b
solbarbera <msolbarbera@gmail.com>
2023-08-25 22:51:12 +0000

    141_v004

--- src/rpart/141_gridsearch_esqueleto_001.r
+++ src/rpart/141_gridsearch_esqueleto_001.r
@@ -103,7 +103,7 @@ dataset <- dataset[clase_ternaria != ""]
 # HT  representa  Hiperparameter Tuning
 dir.create("./exp/", showWarnings = FALSE)
 dir.create("./exp/HT2020/", showWarnings = FALSE)
-archivo_salida <- "./exp/HT2020/gridsearch2.txt"
+archivo_salida <- "./exp/HT2020/gridsearch4.txt"
 
 # Escribo los titulos al archivo donde van a quedar los resultados
 # atencion que si ya existe el archivo, esta instruccion LO SOBREESCRIBE,
@@ -114,6 +114,8 @@ cat(
   sep = "",
   "max_depth", "\t",
   "min_split", "\t",
+  "vcp", "\t",
+  "vmin_bucket", "\t",
   "ganancia_promedio", "\n"
 )
 
