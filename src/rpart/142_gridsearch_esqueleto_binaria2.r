53097c37c89fd191a3aec49c3ceb2f121587b96c
solbarbera <msolbarbera@gmail.com>
2023-08-29 16:48:45 +0000

    142_v002

--- src/rpart/142_gridsearch_esqueleto_binaria2.r
+++ src/rpart/142_gridsearch_esqueleto_binaria2.r
@@ -89,7 +89,7 @@ ArbolesMontecarlo <- function(semillas, param_basicos) {
 #------------------------------------------------------------------------------
 
 # Aqui se debe poner la carpeta de la computadora local
-setwd("C:/Users/Sol Barbera/Documents/Lab1") # Establezco el Working Directory
+setwd("~/buckets/b1/") # Establezco el Working Directory
 # cargo los datos
 
 # cargo los datos
